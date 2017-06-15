8212ad7c8e3275d82d463eb335d4e1b82eebe99a
Loris <loris.vianellos@gmail.com>
2017-06-15 15:46:23 +0200

    loading sources

--- tool.loader.js
+++ tool.loader.js
@@ -1,24 +1,19 @@
 
 var toolloader = {
     run: function() {
-        console.log('Loading...');
-		Memory.mysources=[];
-		for (var ksp in Game.spawns) {
-				var sou=Game.spawns[ksp].room.find(FIND_SOURCES)
-				for (var kso in sou) {
-					Memory.mysources[kso]={'con':0, 'max':1}
-				}
+		if (!Memory.mysources) {
+			console.log('Loading sources...');
+			Memory.mysources=[];
+			for (var ksp in Game.spawns) {
+					var sou=Game.spawns[ksp].room.find(FIND_SOURCES)
+					for (var kso in sou) {
+						//console.log(JSON.stringify(sou[kso]))
+						Memory.mysources[kso]={'pos':sou[kso].pos, 'id':sou[kso].id}
+					}
+			}
+			console.log(JSON.stringify(Memory.mysources))
 		}
-
-		if (!Memory.maxcreep) {
-			var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-		}
-
-
-
-		console.log(JSON.stringify(Memory.mysources))
     }
-
 }
 
 module.exports = toolloader
